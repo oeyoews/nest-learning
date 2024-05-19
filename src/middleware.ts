862211809137a3b7784dba80e818df3bac270709
oeyoews <jyao4783@gmail.com>
2024-05-19 23:05:50 +0800

    update mid

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,28 @@
 import * as emoji from 'node-emoji';
 import { Logger } from '@nestjs/common';
-
-const logger = new Logger('日志');
+import { Request, Response, NextFunction } from 'express';
 
 const chalk = require('chalk');
 
 export default function MiddlewareAll() {
-  return (req, res, next) => {
-    logger.log(chalk.green.bold.underline(`${req.url}`));
+  const logger = new Logger('日志(HTTP)');
+
+  return (req: Request, res: Response, next: NextFunction) => {
+    const { ip, method, originalUrl } = req;
+    // const userAgent = req.get['user-agent'] || '';
+
+    res.on('finish', () => {
+      const { statusCode } = res;
+      const color =
+        statusCode >= 500 ? 'bgRed' : statusCode >= 400 ? 'yellow' : 'green';
+      const emojiStatus =
+        statusCode >= 500 ? '🔥' : statusCode >= 400 ? '⚠️' : '✅';
+      logger.log(
+        chalk.green.bold.underline(`${originalUrl} ${method}`),
+        `${emoji.emojify(emojiStatus)} ${chalk[color](statusCode)}`,
+      );
+    });
+
     next();
   };
 }
