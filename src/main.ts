2abada1f732656acd45dfbe93431ac2218551171
oeyoews <jyao4783@gmail.com>
2024-05-19 10:26:43 +0800

    feat: add node-emoji and chalk

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as emoji from 'node-emoji';
+const chalk = require('chalk');
 
 /** The entry file of the application which uses the core function NestFactory to create a Nest application instance.*/
 async function bootstrap() {
   const port = 3000;
   const app = await NestFactory.create(AppModule);
+
+  // emojis
+  const rocket = emoji.get('rocket');
+  const book = emoji.get('unicorn');
+  const pizza = emoji.get('pizza');
+
   app.setGlobalPrefix('api');
 
   const config = new DocumentBuilder()
@@ -25,7 +33,20 @@ async function bootstrap() {
   });
 
   await app.listen(port);
-  console.log(`Application is running on: ${await app.getUrl()}/swagger`);
+  console.log('\n');
+  console.log(
+    chalk.blue(`${rocket} API is running on: ${await app.getUrl()}/api`),
+  );
+  console.log(
+    chalk.cyan(
+      `${book} Swagger JSON is running on: ${await app.getUrl()}/swagger.json`,
+    ),
+  );
+  console.log(
+    chalk.green.bold(
+      `${pizza} Swagger is running on: ${await app.getUrl()}/swagger`,
+    ),
+  );
 }
 
 bootstrap();
